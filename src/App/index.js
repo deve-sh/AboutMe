ed5337625e86e0cf3366a937d19e2ff6ff5d42d7
Devesh Kumar <devesh2027@gmail.com>
2021-08-02 22:31:24 +0530

    Setup material ui global config theme.

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,22 @@ import { Switch, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
+// Global material ui configuration.
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { teal, deepOrange } from "@material-ui/core/colors";
+
 // Constants
 import constants from "./constants";
 import toasts from "./constants/toastConstants";
 
 import auth from "../firebase/authentication";
+
+const theme = createTheme({
+	palette: {
+		primary: teal,
+		secondary: deepOrange,
+	},
+});
 
 const App = (props) => {
 	const dispatch = useDispatch();
@@ -18,12 +29,12 @@ const App = (props) => {
 
 	// Actual app.
 	return (
-		<React.Fragment>
+		<ThemeProvider theme={theme}>
 			{/* Global Toast for error messages */}
 			<ToastContainer />
 
 			<Switch></Switch>
-		</React.Fragment>
+		</ThemeProvider>
 	);
 };
 export default App;

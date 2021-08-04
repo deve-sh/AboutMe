6c6fcd7c8eb4927f9cb4ab3eb2d2ff6b3a1b270a
Devesh Kumar <devesh2027@gmail.com>
2021-08-04 10:21:34 +0530

    Added getStatus.json serverless function

---
+++ api/getStatus.json.js
@@ -0,0 +1,35 @@
+module.exports = async (req, res) => {
+	try {
+		const firebase = require("../serverSideFirebase");
+
+		let { identifier } = req.query;
+		if (!identifier)
+			return res.status(400).send({
+				error: "Incomplete information provided.",
+			});
+
+		let userInfo = await firebase
+			.firestore()
+			.collection("registeredusers")
+			.doc(identifier)
+			.get();
+
+		if (!userInfo.exists || userInfo.data().disabled)
+			return res.status(404).send({
+				error: "User not found.",
+			});
+
+		userInfo = userInfo.data();
+
+		return res.json({
+			identifier,
+			isStatusPresent: !!userInfo.status || false,
+			status: userInfo.status || "No Status",
+			statusEmoji: userInfo.statusEmoji || "😁",
+		});
+	} catch (err) {
+		return res.status(500).json({
+			message: err.message || "Something went wrong. Please try again later.",
+		});
+	}
+};

ffe1d2e2c0b66259aa20de7ac83eba9dea603e57
Devesh Kumar <devesh2027@gmail.com>
2021-08-05 21:39:27 +0530

    Added appropriate firebase auth mounter for app.

--- src/App/index.js
+++ src/App/index.js
@@ -18,6 +18,13 @@ import auth from "../firebase/authentication";
 // Pages
 import Login from "./pages/Login";
 
+// Store actions
+import {
+	logoutUser as logoutUserFromStore,
+	updateUserDetails,
+} from "./store/actionCreators";
+import { createUserDocument, getUserDocument } from "./API";
+
 const theme = createTheme({
 	palette: {
 		primary: teal,
@@ -25,11 +32,49 @@ const theme = createTheme({
 	},
 });
 
-const App = (props) => {
+const App = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		auth.onAuthStateChanged(console.log);
+		auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				let displayName = user.displayName,
+					email = user.email,
+					emailVerified = user.emailVerified,
+					photoURL = user.photoURL,
+					isAnonymous = user.isAnonymous,
+					phoneNumber = user.phoneNumber,
+					uid = user.uid,
+					providerData = user.providerData,
+					lastSignInTime = user.metadata.lastSignInTime;
+
+				let userData = {
+					identifier: email || phoneNumber,
+					displayName,
+					email,
+					emailVerified,
+					photoURL,
+					isAnonymous,
+					phoneNumber,
+					uid,
+					id: uid,
+					providerData: JSON.parse(JSON.stringify(providerData)),
+					lastSignInTime,
+				};
+
+				// Check if user exists in the database.
+				let userDocumentInDatabase = await getUserDocument(
+					email || phoneNumber
+				);
+				if (userDocumentInDatabase) {
+					// Update any details that might have updated.
+					await updateUserDetails(email || phoneNumber, userData);
+				} else {
+					// Create a user with this identifier.
+					await createUserDocument(email || phoneNumber, userData);
+				}
+			} else dispatch(logoutUserFromStore());
+		});
 	}, []);
 
 	// Actual app.

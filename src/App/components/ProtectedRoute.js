2b6bbd73ad91f9daaa73a9c2b0b30c46da0a46e0
Devesh Kumar <devesh2027@gmail.com>
2021-08-09 19:16:01 +0530

    Updated Protected Route component.

--- src/App/components/ProtectedRoute.js
+++ src/App/components/ProtectedRoute.js
@@ -1,23 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
-import { getToken } from "../../tools/helpers";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const isAuthenticated = useSelector(state => state.isAuthenticated);
+	const isAuthenticated = useSelector((state) => state.isAuthenticated);
+	const user = useSelector((state) => state.user);
 
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        isAuthenticated && getToken() ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/" />
-        )
-      }
-    />
-  );
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				isAuthenticated && user ? <Component {...props} /> : <Redirect to="/" />
+			}
+		/>
+	);
 };
 
 export default ProtectedRoute;

d8ebd2dea1e932ea66599abda905b1945dc6ab00
Devesh Kumar <devesh2027@gmail.com>
2021-08-12 14:28:34 +0530

    Added React Helmet for Page Titles

--- src/App/pages/Profile.js
+++ src/App/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 
 import {
 	Button,
@@ -136,6 +137,9 @@ const Profile = () => {
 
 	return (
 		<ProfileContainer>
+			<Helmet>
+				<title>AboutMe - Profile</title>
+			</Helmet>
 			<ProfilePictureContainer>
 				<ProfilePicture
 					src={state.user?.photoURL || "/defaultprofilephoto.jpeg"}

62793447a4fa6eb70f670ec6540e34481162389d
Devesh Kumar <devesh2027@gmail.com>
2021-08-14 11:59:25 +0530

    Setup home page styling and some content

--- src/App/pages/Home.js
+++ src/App/pages/Home.js
@@ -1,25 +1,53 @@
 import React from "react";
 import styled from "styled-components";
-
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
+
+import { Typography, Button } from "@material-ui/core";
 import { MeetingRoom as LoginIcon } from "@material-ui/icons";
+
+import Image from "../components/Image";
 
 const HomePageHeader = styled.header`
 	text-align: right;
 	display: flex;
 	align-items: center;
 	justify-content: flex-end;
-	padding: 0.875rem;
-	border-bottom: 0.075rem solid #cfcfcf;
+	padding: 0.625rem;
 	text-decoration: none;
 `;
-const HomePageFirstSection = styled.section``;
-const HomePageSecondSection = styled.section``;
+const HomePageFirstSection = styled.section`
+	max-width: 1100px;
+	margin: 0 auto;
+	text-align: center;
+`;
+const HomePageImageSection = styled.div`
+	padding: 1rem;
+
+	img {
+		max-width: 350px;
+	}
+`;
+const HomePageTextSection = styled(HomePageImageSection)``;
+
+const Heading = styled.h1`
+	margin-bottom: 0.5rem;
+`;
+const Description = styled.div`
+	color: #606060;
+	max-width: 650px;
+	margin: 0 auto;
+`;
+
+// Second section styling.
+const HomePageSecondSection = styled(HomePageFirstSection)``;
 
 const Home = (props) => {
 	return (
 		<>
+			<Helmet>
+				<title>AboutMe</title>
+			</Helmet>
 			<HomePageHeader>
 				<Link to="/login" title="Login">
 					<Button color="primary" variant="contained" startIcon={<LoginIcon />}>
@@ -27,8 +55,21 @@ const Home = (props) => {
 					</Button>
 				</Link>
 			</HomePageHeader>
-			<HomePageFirstSection />
-			<HomePageSecondSection />
+			<HomePageFirstSection>
+				<HomePageImageSection>
+					<Image src="/status.svg" alt="Status" />
+				</HomePageImageSection>
+				<HomePageTextSection>
+					<Heading>
+						Tired Of Status Updates? Or Profile Picture Updates?
+					</Heading>
+					<Description>
+						What if there was a service that would allow you to update your
+						status at one place, and have it update everywhere. Magically!
+					</Description>
+				</HomePageTextSection>
+			</HomePageFirstSection>
+			<HomePageSecondSection></HomePageSecondSection>
 		</>
 	);
 };
